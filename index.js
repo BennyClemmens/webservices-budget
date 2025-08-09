1f90724f1b872d99aea6df272143cb89655841f3
Benny Clemmens <benny.clemmens@student.hogent.be>
2025-08-09 22:37:07 +0200

    middlewares basic

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Koa = require('koa'); // old import style
 const app = new Koa();
 
-app.use(async (context) => {
+app.use(async (context, next) => {
   context.body = 'Hello World';
+  await next();
 });
+
+app.use(async (context, next) => {
+  console.log(context)
+  return next; // no use to wait, there is nog next ...
+})
 
 app.listen(9000);  // shorthand for creating ...
 
