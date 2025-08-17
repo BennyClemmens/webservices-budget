2c9a5ddf5a59f4ca4f0ff213947249b4ed0c97e8
Benny Clemmens <benny.clemmens@student.hogent.be>
2025-08-17 11:24:48 +0200

    prisma client added

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,39 @@ import bodyParser from 'koa-bodyparser';
 import installRest from './rest/parent.rest';
 import config from 'config';
 import koaCors from '@koa/cors';
+import { initializeData } from './data';
 
 const CORS_ORIGINS = config.get<string[]>('cors.origins');
 const CORS_MAX_AGE = config.get<number>('cors.maxAge');
 
-const app = new Koa();
+async function main() {
+  const app = new Koa();
 
-app.use(
-  koaCors({
-    origin: (context) => {
-      if (CORS_ORIGINS.indexOf(context.request.header.origin!) !== -1) {
-        return context.request.header.origin!;
-      }
-      // Not a valid domain at this point, let's return the first valid as we should return a string
-      return CORS_ORIGINS[0] || '';
-    },
-    allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
-    maxAge: CORS_MAX_AGE,
-  }),
-);
+  app.use(
+    koaCors({
+      origin: (context) => {
+        if (CORS_ORIGINS.indexOf(context.request.header.origin!) !== -1) {
+          return context.request.header.origin!;
+        }
+        // Not a valid domain at this point, let's return the first valid as we should return a string
+        return CORS_ORIGINS[0] || '';
+      },
+      allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
+      maxAge: CORS_MAX_AGE,
+    }),
+  );
 
-app.use(bodyParser());
+  app.use(bodyParser());
 
-installRest(app);
-// je zou hier nog andere interfaces voor andere clients kunnen bij installeren die dezelfde servicelaag aanspreken
+  await initializeData();
 
-app.listen(9000, () => {
-  getLogger().info('🚀 Server listening on http://127.0.0.1:9000');
-});
+  installRest(app);
+  // je zou hier nog andere interfaces voor andere clients kunnen bij installeren die dezelfde servicelaag aanspreken
+
+  app.listen(9000, () => {
+    getLogger().info('🚀 Server listening on http://127.0.0.1:9000');
+  });
+}
+
+main();
+
