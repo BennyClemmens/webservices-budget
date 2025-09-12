d6ab9a89c4d453764e82fca8e633d2d29e52212d
Benny Clemmens <benny.clemmens@student.hogent.be>
2025-09-12 08:34:38 +0200

    refactor requestlogger middleware

--- src/core/middlewares/requestlogger.ts
+++ src/core/middlewares/requestlogger.ts
@@ -2,30 +2,30 @@ import type { KoaApplication } from '../../types/koa';
 import { getLogger } from '../../core/logging';
 import config from 'config';
 
-const NODE_ENV = config.get<string>('env');
+const NODE_ENV: string = config.get<string>('env');
+const isDev: boolean = (NODE_ENV === 'development');
+
+function stateEmoji(state: number): string {
+  return (!isDev) ? '' :
+    (state >= 500) ? '💀 ' :
+      (state >= 400) ? '❌ ' :
+        (state >= 300) ? '🔀 ' :
+          (state >= 200) ? '✅ ' : '🔄 ';
+};
 
 export default function installRequestLogger(app: KoaApplication) {
   app.use(async (ctx, next) => {
-    let directionEmoji = (NODE_ENV === 'development') ? '⏩' : 'REQUEST';
-    
-    const start = Date.now();
-    getLogger().info(`${directionEmoji} ${ctx.method} ${ctx.url}`);
+    let logPrefix: string = isDev ? '⏩' : 'REQUEST';
+    const start: number = Date.now();
+    getLogger().info(`${logPrefix} ${ctx.method} ${ctx.url}`);
 
     await next();
 
-    const ms = Date.now() - start;
-    const stateEmoji = (() => {
-      if (NODE_ENV !== 'development') return '';
-      if (ctx.status >= 500) return '💀 ';
-      if (ctx.status >= 400) return '❌ ';
-      if (ctx.status >= 300) return '🔀 ';
-      if (ctx.status >= 200) return '✅ ';
-      return '🔄 ';
-    })();
-
-    directionEmoji = (NODE_ENV === 'development') ? '⏪' : 'RESPONSE';
+    const ms: number = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+    logPrefix = isDev ? '⏪' : 'RESPONSE';
     getLogger().info(
-      `${directionEmoji} ${stateEmoji}${ctx.status} (${ctx.method} ${ctx.url}) - ${ms}ms`,
+      `${logPrefix} ${stateEmoji(ctx.status)}${ctx.status} (${ctx.method} ${ctx.url}) - ${ms}ms`,
     );
   });
 }
